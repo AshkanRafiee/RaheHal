85cdd194311d8a73c0bbd9a8bb3ae4caee370e5b
Ashkan <ashkanrafiee@protonmail.com>
2019-05-08 22:08:00 +0430

    Added GUI!

--- linux.sh
+++ linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #made by AshkanRafiee
 #github: https://github.com/AshkanRafiee/No-Sanction
-echo Enter "y" to turn on / Enter every word but "y" to turn off:
-read state
-
-if [ "$state" == "y" ]
+dialog --backtitle "No-Sanction" \
+       --title "Active/Deactive" \
+       --yesno "\nTurn On/Off?\n yes=On  no=Off" 8 30
+if [ "$?" == "0" ]
 then
 	sed -i 's/nameserver 94.232.174.194//g' /etc/resolv.conf
 	sed -i 's/nameserver 178.22.122.100//g' /etc/resolv.conf
@@ -12,12 +12,13 @@ then
 	#3 above lines removes existing nameservers if there is any!
 	sed -i '1 i\nameserver 94.232.174.194\nnameserver 178.22.122.100' /etc/resolv.conf
 	#adds the nameservers
-	echo No-Sanction has been turned ON!
+	dialog --title "Sucessful!" --msgbox 'No-Sanction has been turned ON!' 6 30
 else
 	sed -i 's/nameserver 94.232.174.194//g' /etc/resolv.conf
 	sed -i 's/nameserver 178.22.122.100//g' /etc/resolv.conf
 	sed -i '/^$/d' /etc/resolv.conf
 	#3 above lines removes existing nameservers if there is any!
-	echo No-Sanction has been turned OFF!
+	dialog --title "Sucessful!" --msgbox 'No-Sanction has been turned OFF!' 6 30
 fi
-read -n1 -r -p "Press any key to continue..."
+clear
+exit
